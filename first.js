62f22b0085edfcdc5987bac31aa8ff2b5b6f396d
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-01-13 17:13:44 +0530

    My dream City in HTML project1

---
+++ first.js
@@ -0,0 +1,17 @@
+//artimatic operators
+let a= 5;
+let b=2;
+console.log("a=",a, "& b = ",b);
+
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b)
+//unary 
+
+
+console.log("a++=",a++);
+console.log("a=",a);
+//assignment operator
+a+=4;
